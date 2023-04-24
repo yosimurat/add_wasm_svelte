5cba9eec11eb3a0627eec01f27b1cf6f5eb88329
Toshiyuki Yoshimura <yosimurat@gmail.com>
2023-04-24 13:59:47 +0900

    feat: AddView components

---
+++ src/lib/components/AddView.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import wasm from '@yosimurat/add_wasm';
+
+	export let a: number;
+	export let b: number;
+
+	let value: number = 0;
+
+	(wasm as any)({}).then(({ instance }: any) => {
+		value = instance.exports.add(a, b);
+	});
+</script>
+
+<div>
+	{value}
+</div>
